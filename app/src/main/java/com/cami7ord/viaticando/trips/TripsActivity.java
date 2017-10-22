8b95f834e2063a26599dd437213c1d3f6b9c6bb6
cami7ord <cami7ord@gmail.com>
2017-10-22 02:29:18 -0500

    Visual improvements.
    Visual improvements.

--- app/src/main/java/com/cami7ord/viaticando/trips/TripsActivity.java
+++ app/src/main/java/com/cami7ord/viaticando/trips/TripsActivity.java
@@ -2,6 +2,7 @@ package com.cami7ord.viaticando.trips;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -29,6 +30,7 @@ public class TripsActivity extends BaseActivity {
 
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
         List<String> trips = new ArrayList<>();
         trips.add("A");
