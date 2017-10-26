22c8054fd87207ebc103ff10f3ea314c0c8deab0
cami7ord <cami7ord@gmail.com>
2017-10-25 20:00:54 -0500

    Expenses layout done.
    Expenses layout done. Still needs to add an actual receipt.

--- app/src/main/java/com/cami7ord/viaticando/expenses/ExpensesActivity.java
+++ app/src/main/java/com/cami7ord/viaticando/expenses/ExpensesActivity.java
@@ -4,17 +4,28 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.cami7ord.viaticando.BaseActivity;
 import com.cami7ord.viaticando.R;
 
-public class ExpensesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpensesActivity extends BaseActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -30,6 +41,21 @@ public class ExpensesActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mRecyclerView = findViewById(R.id.trip_detail_recycler);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
+        List<String> expenses = new ArrayList<>();
+        expenses.add("A");
+        expenses.add("B");
+        expenses.add("C");
+        mAdapter = new ExpensesAdapter(this, expenses);
+        mRecyclerView.setAdapter(mAdapter);
+
     }
 
     @Override
---
+++ app/src/main/java/com/cami7ord/viaticando/expenses/ExpensesAdapter.java
@@ -0,0 +1,75 @@
+package com.cami7ord.viaticando.expenses;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.cami7ord.viaticando.R;
+
+import java.util.List;
+
+public class ExpensesAdapter extends RecyclerView.Adapter<ExpensesAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List mDataset;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        RelativeLayout mExpenseLayout;
+        TextView mExpenseName;
+        TextView mExpenseCategory;
+        TextView mExpenseStatus;
+        TextView mExpenseAmount;
+
+        ViewHolder(View v) {
+            super(v);
+            mExpenseLayout = v.findViewById(R.id.expense_layout);
+            mExpenseName = v.findViewById(R.id.expense_name);
+            mExpenseCategory = v.findViewById(R.id.expense_category);
+            mExpenseStatus = v.findViewById(R.id.expense_status);
+            mExpenseAmount = v.findViewById(R.id.expense_amount);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    ExpensesAdapter(Context context, List myDataset) {
+        mContext = context;
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ExpensesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                      int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_trip_expenses, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        return new ExpensesAdapter.ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ExpensesAdapter.ViewHolder holder, int position) {
+        //holder.mTripName
+        //holder.mTextView.setText(mDataset[position]);
+        holder.mExpenseLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent expenses = new Intent(mContext, ExpensesActivity.class);
+                //mContext.startActivity(expenses);
+            }
+        });
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
